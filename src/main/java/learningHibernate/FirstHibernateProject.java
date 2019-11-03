0ea06ae847caa37a0c4e3814bac6ce3647fde698
Madhan-Kumar-Gali <gmadhang9@gmail.com>
2019-11-04 00:03:32 +0530

    second milestone how to use different annotations and create multiple users using multiple sessions and separe address class

--- src/main/java/learningHibernate/FirstHibernateProject.java
+++ src/main/java/learningHibernate/FirstHibernateProject.java
@@ -1,22 +1,54 @@
 package main.java.learningHibernate;
 
+import main.java.learningHibernate.model.Address;
 import main.java.learningHibernate.model.UserDetails;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Date;
+
 
 public class FirstHibernateProject {
 
     public static void main(String[] args) {
+
         UserDetails userDetails = new UserDetails();
-        userDetails.setUserId(1);
+        Address address = new Address();
+        //we are using GeneratedValue in model class so we no need to set primary key id it will be take care by hibernate
+//        userDetails.setUserId(1);
         userDetails.setName("First User");
+        address.setStreet("SarkarThopu");
+        address.setCity("Nellore");
+        address.setState("AndhraPradesh");
+        address.setPincode("524366");
+        userDetails.setAddress(address);
+        userDetails.setJoinedDate(new Date());
+        userDetails.setDescription("Engineer");
+
+        UserDetails userDetails1 = new UserDetails();
+        Address address1 = new Address();
+        userDetails1.setName("Second User");
+        address1.setStreet("Thiparajuvari");
+        address1.setCity("Nellore");
+        address1.setState("AndhraPradesh");
+        address1.setPincode("524001");
+        userDetails1.setAddress(address1);
+        userDetails1.setJoinedDate(new Date());
+        userDetails1.setDescription("Mechanic");
 
         SessionFactory sessionFactory = new Configuration().configure("main/resources/META-INF/hibernate.cfg.xml").buildSessionFactory();
         Session session = sessionFactory.openSession();
         session.beginTransaction();
         session.save(userDetails);
+        session.save(userDetails1);
         session.getTransaction().commit();
+        session.close();
+
+        //Fetch data from database
+        session = sessionFactory.openSession();
+        session.beginTransaction();
+        userDetails = session.get(UserDetails.class, 1);
+        System.out.println("User description retrieved is " +userDetails.getUserId());
     }
 }
--- src/main/java/learningHibernate/model/UserDetails.java
+++ src/main/java/learningHibernate/model/UserDetails.java
@@ -1,15 +1,23 @@
 package main.java.learningHibernate.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+@Table(name = "USER_DETAILS")
 public class UserDetails {
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private int userId;
     private String name;
+    @Embedded
+    private Address address;
+    @Temporal(TemporalType.DATE)
+    private Date joinedDate;
+    @Lob
+    private String description;
 
+    //we can also use annotations near getter @Id,@Column(name="USER_ID) instead of near variables
     public int getUserId() {
         return userId;
     }
@@ -24,5 +32,29 @@ public class UserDetails {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Date getJoinedDate() {
+        return joinedDate;
+    }
+
+    public void setJoinedDate(Date joinedDate) {
+        this.joinedDate = joinedDate;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
